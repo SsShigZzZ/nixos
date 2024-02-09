155a6b9cc35cd3387fa8f572de87aadd1a30c6eb
Matt Pask <pask.matthew.a@gmail.com>
2024-02-09 23:41:40 +0900

    base flake

--- flake.nix
+++ flake.nix
@@ -1,56 +1,27 @@
 {
-  description = "Flakey flake";
+  description = "Matt's home flake";  
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    impermanence.url = "github:nix-community/impermanence";
+  };  
+  
+  outputs = { nixpkgs ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ config, pkgs, ... }:
+              imports =
+                [
+                  ./hosts/nixos
+                ];
+            }
+          )
+        ];
+      };
     };
-
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-  };
-
-  outputs = inputs@{ 
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    home-manager-unstable,
-    hyprland,
-    ... 
-  }:
-  let
-    vars = {
-      user = "matt";
-      location = "$HOME/.setup";
-      terminal = "kitty";
-      editor = "nvim";
-    };
-  in
-  {
-    nixosConfigurations = (
-      import ./hosts {
-        inherit (nixpkgs) lib;
-        inherit 
-          inputs
-          nixpkgs
-          nixpkgs-unstable
-          home-manager
-          hyprland
-          vars
-        ;
-      }
-    );
   };
 }
