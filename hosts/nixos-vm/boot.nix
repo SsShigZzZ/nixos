c924cbef6ffb44f2165333a602dfa46a0a9b126e
Matt Pask <pask.matthew.a@gmail.com>
2024-02-21 15:47:06 +0900

    try different kernel params

--- hosts/nixos-vm/boot.nix
+++ hosts/nixos-vm/boot.nix
@@ -2,7 +2,7 @@
 
 {
   boot = {
-    kernelParams = [ "quiet", "splash"];
+    kernelParams = [ "quiet" "splash"];
     kernelModules = [];
     extraModulePackages = [];
     blacklistedKernelModules = [];
