f2468429feb477176c11a532e721452bf0e29dde
Matt Pask <pask.matthew.a@gmail.com>
2024-02-29 16:46:26 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,34 @@
 {
-  description = "Matt's home flake";  
+  description = "Matt's home flake";
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      inherit (self) outputs;
-      lib = nixpkgs.lib;
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forEachSystem = f: lib.genAttrs systems (sys: f pkgsFor.${sys});
-      pkgsFor = lib.genAttrs systems (system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      });
-    in
-    {
-      inherit lib;
-      nixosModules = import ./modules;
-      overlays = import ./overlays { inherit inputs outputs; };
-      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
-      formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
 
-      nixosConfigurations = {
-        nixos-vm = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./hosts/nixos-vm ];
-        };
+      nixos-vm = lib.nixosSystem {
+        modules = [ ./hosts/nixos-vm ];
+        specialArgs = { inherit inputs outputs; };
       };
+
     };
+  };
   
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    impermanence.url = "github:nix-community/impermanence";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -40,10 +37,21 @@
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    impermanence.url = "github:nix-community/impermanence";
 
-  };  
-  
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
 }
